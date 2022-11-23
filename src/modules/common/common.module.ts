b2e0d1247a52b42f79a705c9fe7ea56bee185e06
Blind Kai <blindkai1426@gmail.com>
2022-11-23 08:34:00 +0200

    Closure to persist env variables at runtime

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,13 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
-import { pinoHttpConfig } from './logger/logger.config';
+import { usePinoHttpConfig } from './logger/logger.config';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    LoggerModule.forRoot({ pinoHttp: pinoHttpConfig }),
+    LoggerModule.forRoot({ pinoHttp: usePinoHttpConfig() }),
   ],
 })
 export class CommonModule {}
--- src/modules/common/logger/logger.config.ts
+++ src/modules/common/logger/logger.config.ts
@@ -1,15 +1,18 @@
 import { randomUUID } from 'crypto';
 import { Options } from 'pino-http';
 
-export const pinoHttpConfig: Options = {
-  timestamp: () => `, "timestamp":"${new Date().toISOString()}"`,
-  genReqId: (req, res: any) => {
-    const requestId = randomUUID();
-    res.setHeader('x-request-id', requestId);
+export const usePinoHttpConfig: () => Options = () => {
+  const debugMode = process.env.LOGGING === 'debug';
 
-    return requestId;
-  },
-  level: process.env.LOGGING === 'debug' ? 'debug' : 'info',
-  transport:
-    process.env.LOGGING === 'debug' ? { target: 'pino-pretty' } : undefined,
+  return {
+    timestamp: () => `, "timestamp":"${new Date().toISOString()}"`,
+    genReqId: (req, res) => {
+      const requestId = randomUUID();
+      res.setHeader('x-request-id', requestId);
+
+      return requestId;
+    },
+    level: debugMode ? 'debug' : 'info',
+    transport: debugMode ? { target: 'pino-pretty' } : undefined,
+  };
 };
